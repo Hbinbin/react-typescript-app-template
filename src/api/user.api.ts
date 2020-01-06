1e4641f51f4185f1e2c14d893feb213d379e9016
haobin <haobin@haobindeMacBook-Pro.local>
2020-01-06 17:10:38 +0800

    package.json修改

--- src/api/user.api.ts
+++ src/api/user.api.ts
@@ -2,8 +2,12 @@ import axios from 'axios';
 import { IGetUserParams } from '@typings/api.typing';
 
 export default class UserAPI {
-  /** 获取用户信息 */
+  /** 获取用户信息API */
   private static readonly USER_API = 'IAccountRoles/getUser';
+  /**
+   * 获取用户信息
+   * @param {string} mobile - 用户手机号
+   */
   static async getUser ({ mobile }: IGetUserParams) {
     const url = 'xxxx';
     const params = {
