f85a339697db433b55aac3cfa837c8c0aac0c64a
haobin <haobin@haobindeMacBook-Pro.local>
2020-01-09 17:50:40 +0800

    utils增加公共方法；增加reselector使用示例

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '@css/hello.scss';
-
+import { addParams } from '@utils/utils';
 class Hello extends Component {
   constructor (props: any) {
     super(props);
--- src/redux/selector.ts
+++ src/redux/selector.ts
@@ -1,1 +1,12 @@
 import { createSelector } from 'reselect';
+
+// 用户信息
+export const getUserInfo = createSelector(
+  [
+    (state: any, props: any) => state.user.name,
+    (state: any, props: any) => state.user.mobile
+  ],
+  (name: string, mobile: string) => {
+    return `${name}：${mobile}`;
+  }
+);
