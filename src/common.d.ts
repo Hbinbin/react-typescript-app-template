a8d5d277947a353ac1f01cddefaee22f413d75a5
haobin <haobin@haobindeMacBook-Pro.local>
2020-01-16 13:47:35 +0800

    去除无用文件

--- src/common.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="./typings/common.d.ts" />
-interface Window {
-  __REDUX_DEVTOOLS_EXTENSION__: Function;
-  __wxjs_environment: any;
-  ENV: any;
-  Config: any;
-}
-
-declare const Config: any;
